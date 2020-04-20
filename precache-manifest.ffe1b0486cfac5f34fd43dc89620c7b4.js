522f2256b5944d979498303183a714a297cb2276
dennokorir <dennokorir@gmail.com>
2020-04-20 12:33:33 +0300

    Updates

---
+++ precache-manifest.ffe1b0486cfac5f34fd43dc89620c7b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d2b006b1f6ad0904d9aa6c75f817e2",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "e122670169161d1c7e7a",
+    "url": "/react-tutorial/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "00451aa6337d6281c9a5",
+    "url": "/react-tutorial/static/js/2.0354376c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.0354376c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e122670169161d1c7e7a",
+    "url": "/react-tutorial/static/js/main.90cfb0f3.chunk.js"
+  },
+  {
+    "revision": "32d96f51efef0a2c7187",
+    "url": "/react-tutorial/static/js/runtime-main.079d628d.js"
+  }
+]);
